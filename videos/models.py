206fff093bb9ecddfcdf0985df8c6a99adb166be
Rodrigo Farinango <farinango2328@saskpolytech.ca>
2024-09-10 19:06:47 -0600

    Added Playlist and Episode models

--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Playlist(models.Model):
+    playlist_id = models.CharField(max_length=255, unique=True)
+    title = models.CharField(max_length=255)
+    season_number = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+class Episode(models.Model):
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE, related_name='episodes')
+    title = models.CharField(max_length=255)
+    video_id = models.CharField(max_length=255, unique=True)
+    thumbnail = models.URLField()
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return self.title
